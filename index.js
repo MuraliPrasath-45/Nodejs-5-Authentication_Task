eeeb649f144f4db08c037b2dd88e4d0eb85f1a01
MuraliPrasath-45 <muraliprasath49964@gmail.com>
2024-05-24 07:57:48 +0530

    express routes done!

---
+++ index.js
@@ -0,0 +1,33 @@
+/** @format */
+
+import express from "express";
+import AppRouter from "./src/routers/router.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = Number(process.env.PORT);
+
+app.use(cors());
+app.use(express.json());
+app.use("/", AppRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Server! 🌐");
+});
+
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017";
+const DB_NAME = process.env.DB_NAME || "password reset";
+
+mongoose
+  .connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+  });
